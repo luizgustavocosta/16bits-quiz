431d5b0a5b220da0ffcd25ef01d3ab203aad83d8
Luiz Gustavo <luiz.gustavo.costa82@gmail.com>
2021-01-28 20:13:44 +0100

    2nd and 3rd master classes

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,15 @@
-import styled from 'styled-components'
+/* eslint-disable react/react-in-jsx-scope */
+import React from 'react';
+import styled from 'styled-components';
 import db from '../db.json';
-import Widget from '../src/components/Widget'
-import QuizBackground from '../src/components/QuizBackground'
-import GitHubCorner from '../src/components/GitHubCorner'
-import Footer from '../src/components/Footer'
+import Widget from '../src/components/Widget';
+import QuizBackground from '../src/components/QuizBackground';
+import GitHubCorner from '../src/components/GitHubCorner';
+import Footer from '../src/components/Footer';
+import { Router, useRouter } from 'next/router';
+import Input from '../src/components/Input';
+import Button from '../src/components/Button';
+
 
 export const QuizContainer = styled.div`
   width: 100%;
@@ -17,7 +23,11 @@ export const QuizContainer = styled.div`
 `;
 
 export default function Home() {
-  return (    
+  // Hooks alwasy before the return
+  const router = useRouter();
+  const [name, setName] = React.useState('');
+  return (
+    // eslint-disable-next-line react/jsx-filename-extension
     <QuizBackground backgroundImage={db.bg}>
       <QuizContainer>
         <Widget>
@@ -25,9 +35,24 @@ export default function Home() {
             <h1>The Star Wars Saga Quiz</h1>
           </Widget.Header>
           <Widget.Content>
-            <p>Teste seu conhecimento desta saga</p>
-            <p>May the force be with you and</p>
-            </Widget.Content>                    
+            <form onSubmit={function (event) {
+              event.preventDefault();
+              router.push(`quiz?name=${name}`);
+              
+            }}>
+              <Input
+                name="userName"
+                onChange={(currentEvent) => {
+                    setName(currentEvent.target.value);
+                }}
+                placeholder="Digite o seu nome"
+                value={name}
+              />
+              <Button type="submit" disabled={name.length === 0}>
+                {`Play ${name}`}
+              </Button>
+            </form>
+           </Widget.Content>
         </Widget>
         <Widget>
           <Widget.Header>
@@ -35,7 +60,7 @@ export default function Home() {
           </Widget.Header>
           <Widget.Content>
             <p>This is the way ...</p>
-          </Widget.Content>                    
+          </Widget.Content>
         </Widget>
         <Footer />
       </QuizContainer>
--- src/components/Widget/index.js
+++ src/components/Widget/index.js
@@ -1,4 +1,5 @@
-import styled from 'styled-components'
+import styled from 'styled-components';
+
 const Widget = styled.div`
   margin-top: 24px;
   margin-bottom: 24px;
@@ -6,6 +7,7 @@ const Widget = styled.div`
   background-color: ${({ theme }) => theme.colors.mainBg};
   border-radius: 4px;
   overflow: hidden;
+
   h1, h2, h3 {
     font-size: 16px;
     font-weight: 700;
@@ -34,7 +36,7 @@ Widget.Header = styled.header`
 Widget.Content = styled.div`
   padding: 24px 32px 32px 32px;
   & > *:first-child {
-    margin-top: 10;
+    margin-top: 0;
   }
   & > *:last-child {
     margin-bottom: 0;
@@ -42,6 +44,24 @@ Widget.Content = styled.div`
   ul {
     list-style: none;
     padding: 0;
+  }
+`;
+
+Widget.Topic = styled.a`
+  outline: 0;
+  text-decoration: none;
+  color: ${({ theme }) => theme.colors.contrastText};
+  background-color: ${({ theme }) => `${theme.colors.primary}40`};
+  padding: 10px 15px;
+  margin-bottom: 8px;
+  cursor: pointer;
+  border-radius: ${({ theme }) => theme.borderRadius};
+  transition: .3s;
+  display: block;
+  
+  &:hover,
+  &:focus {
+    opacity: .5;
   }
 `;
 
